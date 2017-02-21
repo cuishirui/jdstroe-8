ae6b9bc494e52ef848d866f472cd8e3f368b8b87
cuishirui <344494024@qq.com>
2017-02-21 10:50:58 +0800

    touch checkout.html.erb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  validates :billing_name, present: true
+  validates :billing_address, present: true
+  validates :shipping_name, present: true
+  validates :shipping_address, present: true 
 end

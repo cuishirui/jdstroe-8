9c446ba14a881a71348ef6494e9f4ad79a3fa464
cuishirui <344494024@qq.com>
2017-02-21 12:58:36 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   namespace :account do
-    resources :orders 
+    resources :orders
   end
 
   resources :products do
@@ -24,6 +24,11 @@ Rails.application.routes.draw do
   end
 
   resources :cart_items
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat 
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
